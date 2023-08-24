a933d5e072ab8280bbb6b7a6328fe4a9a5a65d78
techStarZx <stechstar.me@gmail.com>
2023-08-24 16:43:56 +0500

    add checkWin()

--- script.js
+++ script.js
@@ -28,3 +28,21 @@ function resetGame() {
     });
     currentPlayer = 'X'
 }
+
+function CheckWin() {
+    const winningCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],  // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],  // Columns
+        [0, 4, 8], [2, 4, 6]              // Diagonal
+    ];
+
+    for (const combination of winningCombinations) {
+        const [a, b, c] = combination;
+        if (cells[a].textContent === cells[b].textContent && 
+            cells[a].textContent === cells[c].textContent) {
+                return true;
+            }
+    }
+
+    return false;
+}
