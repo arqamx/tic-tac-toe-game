80bf62c2e0f8ceea2a57ca0907169ff96c07bfd2
techStarZx <stechstar.me@gmail.com>
2023-08-24 15:12:01 +0500

    add eventListner to .cell

---
+++ script.js
@@ -0,0 +1,19 @@
+const cells = document.querySelectorAll('.cell');
+let currentPlayer = 'X';
+
+cells.forEach(cell => {
+    cell.addEventListener('click', () => {
+        if(!cell.textContent) {
+            cell.textContent = currentPlayer;
+            if(CheckWin()) {
+                alert(`${currentPlayer} wins!`);
+                resetGame();
+            } else if(checkDraw()) {
+                alert('It\'s a draw!');
+                resetGame();
+            } else {
+                currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            }
+        }
+    });
+});
